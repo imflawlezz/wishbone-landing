c088b521a99a2969763b0298a99b75061f23a6fd
imflawlezz <egorart6@gmail.com>
2025-06-30 14:50:25 +0200

    chore: add static data for navigation bar in Header.tsx

---
+++ src/data/NavLinks.ts
@@ -0,0 +1,8 @@
+export const NavLinks = [
+    { label: "Projects", href: "/" },
+    { label: "About", href: "/" },
+    { label: "News", href: "/" },
+    { label: "Team", href: "/" },
+    { label: "Contact", href: "/" },
+    { label: "Get template", href: "/", isButton: true },
+]
