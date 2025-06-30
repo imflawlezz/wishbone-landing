67bd3f9c4c00e53a6b2d69ac81b98d76e8d4bec9
imflawlezz <egorart6@gmail.com>
2025-06-30 19:56:13 +0200

    chore: add static data for "Prior Clients" section

---
+++ src/data/CustomersList.ts
@@ -0,0 +1,20 @@
+import {resolveAsset} from "@/utils/resolveAsset";
+
+export const CustomersList = [
+    {
+        name: "Hill & Hayes",
+        imageURL: resolveAsset('logos', 'hill-hayes-logo.svg')
+    },
+    {
+        name: "Riksgransen",
+        imageURL: resolveAsset('logos', 'riksgransen-logo.svg')
+    },
+    {
+        name: "Studio Cai",
+        imageURL: resolveAsset('logos', 'studio-cai-logo.svg')
+    },
+    {
+        name: "Chill Industries",
+        imageURL: resolveAsset('logos', 'chill-industries-logo.svg')
+    },
+]
