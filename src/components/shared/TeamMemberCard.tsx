cb7bab082bbbf6ffb807c7e909d09883b8361c32
imflawlezz <egorart6@gmail.com>
2025-06-30 16:56:44 +0200

    feat: add TeamMemberCard.tsx component with props

---
+++ src/components/shared/TeamMemberCard.tsx
@@ -0,0 +1,28 @@
+import {TeamMemberCardProps} from "@/types/TeamMemberCardProps";
+import Image from "next/image";
+
+export const TeamMemberCard = ({
+    firstName,
+    lastName,
+    occupation,
+    imageURL,
+    className
+}:TeamMemberCardProps) => {
+    return (
+        <div className={`${className} flex flex-row items-center gap-7`}>
+            <div className="w-20 h-20 relative rounded-full overflow-hidden shadow-lg border-[3px] border-[var(--background-touch)]">
+                <Image
+                    src={imageURL}
+                    alt={`${firstName} ${lastName} profile image`}
+                    fill
+                    objectFit="cover"
+                    className=""
+                />
+            </div>
+            <div className="flex flex-col justify-center">
+                <p className="text-[var(--foreground)]">{firstName} {lastName}</p>
+                <p className="text-[var(--paragraph-secondary)] text-sm">{occupation}</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/types/TeamMemberCardProps.ts
@@ -0,0 +1,7 @@
+export interface TeamMemberCardProps {
+    firstName: string;
+    lastName: string;
+    occupation: string;
+    imageURL: string;
+    className?: string;
+}
