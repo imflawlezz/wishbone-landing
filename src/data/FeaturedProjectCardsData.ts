3b993b14cdb6c46032a3c8398918b282c7a140c2
imflawlezz <egorart6@gmail.com>
2025-07-01 12:58:23 +0200

    chore: add static data for "Featured Projects" section

---
+++ src/data/FeaturedProjectCardsData.ts
@@ -0,0 +1,20 @@
+import {resolveAsset} from "@/utils/resolveAsset";
+
+export const FeaturedProjectCardsData = [
+    {
+        location: "California",
+        name: "The Hive",
+        imageUrl: resolveAsset('featured', 'featured-1.jpeg'),
+    },
+    {
+        location: "New York",
+        name: "Big Road Brewery",
+        imageUrl: resolveAsset('featured', 'featured-2.jpeg'),
+    },
+    {
+        location: "Madrid",
+        name: "Reeding House",
+        imageUrl: resolveAsset('featured', 'featured-3.jpeg'),
+    }
+
+]
