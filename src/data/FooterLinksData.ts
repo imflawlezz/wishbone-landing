4e2ddb90e2f4ffcb5f255e91598b89c20350f524
imflawlezz <egorart6@gmail.com>
2025-07-01 20:12:59 +0200

    chore: add static data for Footer.tsx component

---
+++ src/data/FooterLinksData.ts
@@ -0,0 +1,19 @@
+import {resolveAsset} from "@/utils/resolveAsset";
+
+export const FooterLinksData = [
+    {
+        name: "Twitter",
+        imageURL: resolveAsset('icons', 'twitter-icon.svg'),
+        href: "/"
+    },
+    {
+        name: "Instagram",
+        imageURL: resolveAsset('icons', 'instagram-icon.svg'),
+        href: "/"
+    },
+    {
+        name: "Facebook",
+        imageURL: resolveAsset('icons', 'facebook-icon.svg'),
+        href: "/"
+    },
+]
