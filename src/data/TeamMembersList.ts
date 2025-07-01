94367c95553ddb29c142673f2501b78e30635081
imflawlezz <egorart6@gmail.com>
2025-07-01 13:23:15 +0200

    chore: add static data for "Team" section

---
+++ src/data/TeamMembersList.ts
@@ -0,0 +1,34 @@
+import {resolveAsset} from "@/utils/resolveAsset";
+
+export const TeamMembersList = [
+    {
+        firstName: "Stephen",
+        lastName: "Collier",
+        occupation: "Senior Partner",
+        imageURL: resolveAsset('team', 'stephen-c.jpeg'),
+    },
+    {
+        firstName: "Nolan",
+        lastName: "Peters",
+        occupation: "Associate",
+        imageURL: resolveAsset('team', 'nolan-p.jpeg'),
+    },
+    {
+        firstName: "Ferris",
+        lastName: "Wonder",
+        occupation: "Associate Partner",
+        imageURL: resolveAsset('team', 'ferris-w.jpeg'),
+    },
+    {
+        firstName: "Aria",
+        lastName: "Stone",
+        occupation: "Senior Partner",
+        imageURL: resolveAsset('team', 'aria-s.jpeg'),
+    },
+    {
+        firstName: "Niko",
+        lastName: "Ferry",
+        occupation: "Partner",
+        imageURL: resolveAsset('team', 'niko-f.jpeg'),
+    },
+]
